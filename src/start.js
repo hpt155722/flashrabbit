b48c944e775ad853182f49e89204a39e50974d9c
Jinn Ha <haphu155722@gmail.com>
2023-09-27 10:41:55 -0500

    CREATED SORT BY SET BUTTON

--- src/start.js
+++ src/start.js
@@ -11,6 +11,7 @@ function onload() {
     //HIDE MAIN APP
     document.getElementById('header').style.display = 'none';
     document.querySelector('.entireAlertContainer').style.display = 'none';
+    document.querySelector('.addSetAlertContainer').style.display = 'none';
     document.getElementById('allSetsContainerContainer').style.display = 'none';
 }
 
@@ -70,14 +71,48 @@ function moveToMain() {
     }, 1800);
 }
 
+let setSortBy = 'A-Z';
+document.getElementById('selectSortBy').addEventListener('change', function() {
+    console.log('changed select');
+    var selectedOption = this.value;
+    console.log(selectedOption);
+    if (selectedOption === 'A-Z') {
+        setSortBy = 'A-Z';
+    } else if (selectedOption === 'Most Unlearned') {
+        setSortBy = 'Most Unlearned';
+    }
+    readAndDisplay();
+});
+
+function updateVariable(selectedOption) {
+    // Implement your logic here to update the variable based on the selected option
+}
+
 function readAndDisplay()
 {
+    let total;
+    let learned;
     // Read and display sets from set.json
     fetch('sets.json')
         .then(response => response.json())
         .then(data => {
             const dataArray = data; // Assign the fetched data to dataArray
-            console.log(dataArray); // Verify if the data is read correctly
+            console.log(dataArray); // Verify if the data is read correctly     
+            
+            // Sort Sets
+            if (setSortBy === 'Most Unlearned') {
+                dataArray.sort((a, b) => {
+                    const unlearnedA = a.cards.filter(card => !card.learned).length;
+                    const unlearnedB = b.cards.filter(card => !card.learned).length;
+
+                    console.log('testa');  // Move this line here
+                    return unlearnedB - unlearnedA;
+                });
+            } else { // Sort by A-Z
+                dataArray.sort((a, b) => a.setName.localeCompare(b.setName));
+                console.log('testb');  // Move this line here
+            }
+
 
             // Display each data
             const container = document.getElementById('allSetsContainer'); 
@@ -85,26 +120,42 @@ function readAndDisplay()
                 <div class="setContainer" style = 'background-color: #b5ccff'>
                     <div class = 'horContainer' style = 'height: 100%'>
                         <div class = 'verContainer'>
-                            <h2> New set+ </h2>
+                            <h2 onclick = 'openAddSet()'> New set+ </h2>
                         </div>
                     </div>
                 </div>
             `;
             dataArray.forEach(data => {
-                const div = createDivForData(data);
-                container.prepend(div);
+                const stats = calculateSetStats(dataArray, data.setId);
+                total = stats ? stats.totalCards : 0;
+                learned = stats ? stats.learnedCards : 0;
+
+                const div = createDivForData(data, total, learned);
+                container.appendChild(div); 
             });
+
+        
         })
         .catch(error => console.error('Error:', error));
 }
 
-function createDivForData(data) {
+function calculateSetStats(dataArray, setId) {
+    const set = dataArray.find(set => set.setId === setId);
+    if (!set) return null;
+
+    const totalCards = set.cards.length;
+    const learnedCards = set.cards.filter(card => card.learned).length;
+
+    return { totalCards, learnedCards };
+}
+
+function createDivForData(data, total, learned) {
     const div = document.createElement('div');
     div.classList.add('setContainer'); 
   
     div.innerHTML = `
         <h2> ${data.setName} </h2>
-        <p class='learnedCardsInfo'> Learned: ${data.learnedCards} / ${data.totalCards}</p>
+        <p class='learnedCardsInfo'> Learned: ${learned} / ${total}</p>
         <img onclick="openEditSetName('${data.setName}')" class="setEditIcon" src="icons/edit.png">
     `;
   
@@ -159,7 +210,7 @@ function saveEditSetName() {
             }
         })
         .catch(error => console.error('Error:', error));
-        closeEditSetName()
+        closeEditSetName();
 }
 
 // Close Edit Set Name
@@ -171,5 +222,63 @@ function closeEditSetName() {
     setTimeout(function() {
         entireAlertContainer.classList.remove('fade-out');
         entireAlertContainer.style.display = 'none';
+    }, 500);
+}
+
+// Open Add Set
+function openAddSet() {
+    const addSetAlertContainer = document.querySelector('.addSetAlertContainer');
+    addSetAlertContainer.classList.add('fade-in');
+    addSetAlertContainer.style.display = 'block';
+    
+    setTimeout(function() {
+        addSetAlertContainer.classList.remove('fade-in');
+    }, 500);
+}
+
+// Save Add Set
+function saveAddSet() {
+    const newName = document.getElementById('newNameCreated').value; // Get new name
+
+    try {
+        const data = fs.readFileSync('src/sets.json'); // Read sets.json
+        const dataArray = JSON.parse(data); // Parse JSON data
+
+        // Add a new set with setName as newName to sets.json
+        const newSet = {
+            "setName": newName,
+            "totalCards": 0,
+            "learnedCards": 0
+        };
+        dataArray.push(newSet);
+
+        // Save data to sets.json file
+        try {
+            fs.writeFileSync('src/sets.json', JSON.stringify(dataArray));
+            readAndDisplay();
+            console.log("File written");
+        } catch (err) {
+            console.error(err);
+        }
+
+        // Update the displayed sets
+        readAndDisplay();
+        console.log("File written");
+        
+        // closeAddSetName();
+    } catch (err) {
+        console.error(err);
+    }
+    closeAddSetName();
+}
+
+// Close Add Set
+function closeAddSetName() {
+    const addSetAlertContainer = document.querySelector('.addSetAlertContainer');
+    addSetAlertContainer.classList.add('fade-out');
+
+    setTimeout(function() {
+        addSetAlertContainer.classList.remove('fade-out');
+        addSetAlertContainer.style.display = 'none';
     }, 500);
 }
