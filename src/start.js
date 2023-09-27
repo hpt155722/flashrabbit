c9db7dff9fea6af21b6bbf37eaf6ac5b556ce2eb
Jinn Ha <haphu155722@gmail.com>
2023-09-27 13:22:04 -0500

    CREATED FLASHCARD VIEW

--- src/start.js
+++ src/start.js
@@ -9,10 +9,18 @@ function onload() {
     document.getElementById('leftRabbit').classList.add('slide-top');
 
     //HIDE MAIN APP
-    document.getElementById('header').style.display = 'none';
-    document.querySelector('.entireAlertContainer').style.display = 'none';
+    document.getElementById('setsPageHeader').style.display = 'none';
+    document.querySelector('.editSetAlertContainer').style.display = 'none';
     document.querySelector('.addSetAlertContainer').style.display = 'none';
     document.getElementById('allSetsContainerContainer').style.display = 'none';
+
+    document.getElementById('flashcardPageHeader').style.display = 'none';
+    document.querySelector('.addFlashcardAlertContainer').style.display = 'none';
+    document.querySelector('.editFlashcardAlertContainer').style.display = 'none';
+    document.getElementById('allFlashcardsContainerContainer').style.display = 'none';
+    
+    
+    
 }
 
 function moveToMain() {
@@ -59,20 +67,20 @@ function moveToMain() {
     }, 800);
 
     setTimeout(function() {
-        document.getElementById('header').style.display = 'block';
-        document.getElementById('header').classList.add('slide-in-left');
+        document.getElementById('setsPageHeader').style.display = 'block';
+        document.getElementById('setsPageHeader').classList.add('slide-in-left');
         document.getElementById('allSetsContainerContainer').style.display = 'flex';
         document.getElementById('allSetsContainerContainer').classList.add('slide-in-right');
     }, 900);
 
     setTimeout(function() {
-        document.getElementById('header').classList.remove('slide-in-left');
+        document.getElementById('setsPageHeader').classList.remove('slide-in-left');
         document.getElementById('allSetsContainerContainer').classList.remove('slide-in-right');
     }, 1800);
 }
 
 let setSortBy = 'A-Z';
-document.getElementById('selectSortBy').addEventListener('change', function() {
+document.getElementById('setSortBy').addEventListener('change', function() {
     console.log('changed select');
     var selectedOption = this.value;
     console.log(selectedOption);
@@ -156,8 +164,21 @@ function createDivForData(data, total, learned) {
     div.innerHTML = `
         <h2> ${data.setName} </h2>
         <p class='learnedCardsInfo'> Learned: ${learned} / ${total}</p>
-        <img onclick="openEditSetName('${data.setName}')" class="setEditIcon" src="icons/edit.png">
+        <img id="editIcon-${data.setId}" class="setEditIcon" src="icons/edit.png">
     `;
+
+    //Opens set to view cards
+    div.addEventListener('click', function() {
+        moveToFlashcards(data.setId, data.setName);
+    });
+
+    // Edit Set
+    const editIcon = div.querySelector(`#editIcon-${data.setId}`);
+    editIcon.addEventListener('click', function(event) {
+        event.stopPropagation(); // Stop propagation to prevent the div click event
+        openEditSetName(data.setName);
+    });
+
   
     return div;
 }
@@ -167,12 +188,12 @@ function createDivForData(data, total, learned) {
 let currentOpenedSetToEdit;
 function openEditSetName(setName) {
     currentOpenedSetToEdit = setName;
-    const entireAlertContainer = document.querySelector('.entireAlertContainer');
-    entireAlertContainer.classList.add('fade-in');
-    entireAlertContainer.style.display = 'block';
+    const editSetAlertContainer = document.querySelector('.editSetAlertContainer');
+    editSetAlertContainer.classList.add('fade-in');
+    editSetAlertContainer.style.display = 'block';
     
     setTimeout(function() {
-        entireAlertContainer.classList.remove('fade-in');
+        editSetAlertContainer.classList.remove('fade-in');
     }, 500);
 }
 
@@ -216,12 +237,12 @@ function saveEditSetName() {
 // Close Edit Set Name
 function closeEditSetName() {
     currentOpenedSetToEdit = '';
-    const entireAlertContainer = document.querySelector('.entireAlertContainer');
-    entireAlertContainer.classList.add('fade-out');
+    const editSetAlertContainer = document.querySelector('.editSetAlertContainer');
+    editSetAlertContainer.classList.add('fade-out');
 
     setTimeout(function() {
-        entireAlertContainer.classList.remove('fade-out');
-        entireAlertContainer.style.display = 'none';
+        editSetAlertContainer.classList.remove('fade-out');
+        editSetAlertContainer.style.display = 'none';
     }, 500);
 }
 
@@ -281,4 +302,230 @@ function closeAddSetName() {
         addSetAlertContainer.classList.remove('fade-out');
         addSetAlertContainer.style.display = 'none';
     }, 500);
+}
+
+//FLASHCARD JAVASCRIPT
+
+let currOpenedSetToView;
+function moveToFlashcards(setID, setName) {
+    currOpenedSetToView = setID;
+    document.getElementById('nameOfSetOnFlashcardPage').innerHTML = 'ʚ ♡ ' + setName + ' ♡ ɞ';
+    document.getElementById('setsPageHeader').classList.add('slide-out-top');
+    document.getElementById('allSetsContainerContainer').classList.add('slide-out-left');
+    
+
+    setTimeout(function() {
+        document.getElementById('setsPageHeader').style.display = 'none';
+        document.getElementById('setsPageHeader').classList.remove('slide-out-top');
+        document.getElementById('allSetsContainerContainer').style.display = 'none';
+        document.getElementById('allSetsContainerContainer').classList.remove('slide-out-left');
+
+        readAndDisplayCards(); //PREPARE SETS
+    }, 800);
+
+    setTimeout(function() {
+        document.getElementById('flashcardPageHeader').style.display = 'block';
+        document.getElementById('flashcardPageHeader').classList.add('slide-in-right');
+        document.getElementById('allFlashcardsContainerContainer').style.display = 'flex';
+        document.getElementById('allFlashcardsContainerContainer').classList.add('slide-in-right');
+    }, 900);
+
+    setTimeout(function() {
+        document.getElementById('flashcardPageHeader').classList.remove('slide-in-right');
+        document.getElementById('allFlashcardsContainerContainer').classList.remove('slide-in-right');
+    }, 1800);
+}
+
+function readAndDisplayCards()
+{
+    // Read and display sets from set.json
+    fetch('sets.json')
+        .then(response => response.json())
+        .then(data => { 
+            const set = data.find(item => item.setId === currOpenedSetToView);
+            if (!set) {
+                console.error(`Set with ID ${currOpenedSetToView} not found.`);
+                return;
+            }
+
+            // Sort the cards based on cardSortBy
+            if (cardSortBy === 'Unlearned First') {
+                set.cards.sort((a, b) => (a.learned && !b.learned) ? 1 : -1);
+                
+            } else {
+                set.cards.sort((a, b) => a.question.localeCompare(b.question));
+            }
+
+            // Display each card
+            const container = document.getElementById('allFlashcardsContainer'); // Assuming you have a container element with id 'cardsContainer'
+            container.innerHTML = '';
+
+            set.cards.forEach(card => {
+                const cardDiv = createCardDiv(card);
+                container.appendChild(cardDiv);
+            });
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+function createCardDiv(card) {
+    const cardDiv = document.createElement('div');
+    cardDiv.classList.add('cardContainer'); 
+
+    if (card.learned) {
+        cardDiv.classList.add('learned'); // Add a class if learned is true
+    }
+  
+    cardDiv.innerHTML = `
+        <h3> ${card.question} </h3>
+        <h4> ${card.answer} </p>
+        <p class = 'learnedText'> Learned: ${card.learned ? '✓' : 'X'} </p>
+        <img onclick="openEditCard(${card.cardId})" class="setEditIcon" src="icons/edit.png">
+    `;
+
+    return cardDiv;
+}
+
+let cardSortBy = 'A-Z';
+document.getElementById('flashcardSortBy').addEventListener('change', function() {
+    console.log('changed select');
+    var selectedOption = this.value;
+    console.log(selectedOption);
+    if (selectedOption === 'A-Z') {
+        cardSortBy = 'A-Z';
+    } else if (selectedOption === 'Unlearned First') {
+        cardSortBy = 'Unlearned First';
+    }
+    readAndDisplayCards();
+});
+
+// Open Edit Card
+let currentOpenedCardToEdit;
+async function openEditCard(cardID) {
+    currentOpenedCardToEdit = cardID;
+    const editFlashcardAlertContainer = document.querySelector('.editFlashcardAlertContainer');
+    editFlashcardAlertContainer.classList.add('fade-in');
+    editFlashcardAlertContainer.style.display = 'block';
+
+    //SET CURRENT EDITS AS JSON DATA
+    const frontOfCardText = document.getElementById('changedCardQuestion');
+    const backOfCardText = document.getElementById('changedCardAnswer');
+    
+    try {
+        const card = await findCardById(currOpenedSetToView, cardID);
+        if (card) {
+            frontOfCardText.value = card.question;
+            backOfCardText.value = card.answer;
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+
+    setTimeout(function() {
+        editFlashcardAlertContainer.classList.remove('fade-in');
+    }, 500);
+}
+
+// Function to find a card by its ID
+function findCardById(setId, cardId) {
+    return fetch('sets.json')
+        .then(response => response.json())
+        .then(data => { 
+            const set = data.find(item => item.setId === setId);
+            if (!set) {
+                console.error(`Set with ID ${setId} not found.`);
+                return null;
+            }
+
+            const card = set.cards.find(card => card.cardId === cardId);
+            return card || null;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            throw error; // Re-throw the error to be caught by the caller
+        });
+}
+
+// Save Edit Card
+function saveEditFlashcard() {
+    const changedCardQuestion = document.getElementById('changedCardQuestion').value; // Get Question
+    const changedCardAnswer = document.getElementById('changedCardAnswer').value; // Get Answer
+
+    console.log('currOpenedSetToView: ', currOpenedSetToView);
+    console.log('currentOpenedCardToEdit: ', currentOpenedCardToEdit);
+
+    fetch('sets.json')
+        .then(response => response.json())
+        .then(data => {
+            const dataArray = data; // Assign the fetched data to dataArray
+
+            // Find the set and card
+            const set = dataArray.find(item => item.setId === currOpenedSetToView);
+            if (!set) {
+                console.error(`Set with ID ${currOpenedSetToView} not found.`);
+                return;
+            }
+
+            const card = set.cards.find(card => card.cardId === currentOpenedCardToEdit);
+            if (!card) {
+                console.error(`Card with ID ${currentOpenedCardToEdit} not found in set ${set.setId}.`);
+                return;
+            }
+
+            // Update card question and answer
+            card.question = changedCardQuestion;
+            card.answer = changedCardAnswer;
+
+            // Save data to sets.json file
+            try {
+                fs.writeFileSync('src/sets.json', JSON.stringify(dataArray));
+                readAndDisplayCards();
+                console.log("File written");
+            } catch (err) {
+                console.error(err);
+            }
+        })
+        .catch(error => console.error('Error:', error));
+
+    closeEditFlashcard();
+}
+
+
+// Close Edit Card
+function closeEditFlashcard() {
+    const editFlashcardAlertContainer = document.querySelector('.editFlashcardAlertContainer');
+    editFlashcardAlertContainer.classList.add('fade-out');
+
+    setTimeout(function() {
+        editFlashcardAlertContainer.classList.remove('fade-out');
+        editFlashcardAlertContainer.style.display = 'none';
+    }, 500);
+}
+
+function backToSets()
+{
+    document.getElementById('flashcardPageHeader').classList.add('slide-out-right');
+    document.getElementById('allFlashcardsContainerContainer').classList.add('slide-out-right');
+    
+
+    setTimeout(function() {
+        document.getElementById('flashcardPageHeader').style.display = 'none';
+        document.getElementById('flashcardPageHeader').classList.remove('slide-out-right');
+        document.getElementById('allFlashcardsContainerContainer').style.display = 'none';
+        document.getElementById('allFlashcardsContainerContainer').classList.remove('slide-out-right');
+
+        readAndDisplay(); //PREPARE SETS
+    }, 800);
+
+    setTimeout(function() {
+        document.getElementById('setsPageHeader').style.display = 'block';
+        document.getElementById('setsPageHeader').classList.add('slide-in-left');
+        document.getElementById('allSetsContainerContainer').style.display = 'flex';
+        document.getElementById('allSetsContainerContainer').classList.add('slide-in-left');
+    }, 900);
+
+    setTimeout(function() {
+        document.getElementById('setsPageHeader').classList.remove('slide-in-left');
+        document.getElementById('allSetsContainerContainer').classList.remove('slide-in-left');
+    }, 1800);
 }
